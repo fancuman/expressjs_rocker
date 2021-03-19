32333ddc2bd28cf5ef067a2611d0206361f03258
fancuman <fancuman@gmail.com>
2021-03-19 11:27:00 +1100

    add router module

--- test/index.js
+++ test/index.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
     res.send('Hello World!')
 })
 
+app.get('/good', function (req, res) {
+    res.send('good')
+})
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 })
