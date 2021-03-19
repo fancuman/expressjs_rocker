d12dfc50eb3d7f4b714d40346b666bcc2e65bc47
fancuman <fancuman@gmail.com>
2021-03-19 17:02:51 +1100

    middle for different layer

--- test/index.js
+++ test/index.js
@@ -1,6 +1,5 @@
 const expross = require('../');
 const app = expross();
-// var router = expross.Router();
 
 // router.use((req,res,next)=>{
 //     console.log("Time:", Date.now())
@@ -10,36 +9,45 @@ app.use('/good', (req, res, next) => {
     next();
 })
 
-app.get('/', function (req, res, next) {
-    next();
-})
+// app.get('/', function (req, res, next) {
+//     next();
+// })
 
-    .get('/', function (req, res, next) {
-        console.log("before error:")
-        next();
-        // console.log(d)
-        // next(new Error('customer error'));
-    })
+//     .get('/', function (req, res, next) {
+//         // console.log("before error:")
+//         next();
+//         // console.log(d)
+//         // next(new Error('customer error'));
+//     })
 
-    .get('/', function (req, res) {
-        res.send('third');
-    });
+//     .get('/', function (req, res) {
+//         res.send('third');
+//     });
 
-app.get('/good', function (req, res, next) {
+app.get('/good/', function (req, res, next) {
     // res.send('get');
     next();
-}).get('/good', function (req, res, next) {
+}).get('/good/', function (req, res, next) {
     res.send('get2');
 });
 
-app.put('/good', function (req, res, next) {
+app.put('/good2', function (req, res, next) {
     res.send('put');
     next();
 })
-app.post('/good', function (req, res, next) {
+app.post('/good3', function (req, res, next) {
     res.send('post');
     next();
 })
+
+var router = expross.Router();
+router.use('/1', function (req, res, next) {
+    res.send('first user');
+});
+router.use('/2', function (req, res, next) {
+    res.send('second user');
+});
+app.use('/users', router);
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
